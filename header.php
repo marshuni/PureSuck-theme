d2f10ad6ec4061de1251c441210b3fce65b58eca
marshuniverse <1261565515@outlook.com>
2025-04-19 20:51:43 +0800

    增加LaTeX公式渲染支持 - 基于KaTeX

--- header.php
+++ header.php
@@ -123,12 +123,20 @@
     <link href="<?php $this->options->themeUrl('/css/PureSuck_Module.css'); ?>" rel="stylesheet">
     <link href="<?php getStaticURL('aos.css'); ?>" rel="stylesheet">
     <link defer href="<?php $this->options->themeUrl('/css/MoxDesign.css'); ?>" rel="stylesheet">
+    <?php if ($this->is('post') && $this->fields->isLatex == 1): ?>
+        <link defer href="<?php $this->options->themeUrl('/katex.min.css'); ?>" rel="stylesheet"/>
+    <?php endif; ?>
     <!-- JS引入 -->
     <script defer src="<?php getStaticURL('medium-zoom.min.js'); ?>"></script>
     <script defer src="<?php getStaticURL('highlight.min.js'); ?>"></script>
     <script defer src="<?php $this->options->themeUrl('/js/PureSuck_Module.js'); ?>"></script>
     <script defer src="<?php $this->options->themeUrl('/js/OwO.min.js'); ?>"></script>
     <script defer src="<?php $this->options->themeUrl('/js/MoxDesign.js'); ?>"></script>
+    <?php if ($this->is('post') && $this->fields->isLatex == 1): ?>
+        <script defer src="<?php $this->options->themeUrl('/js/katex.min.js'); ?>"></script>
+        <script defer src="<?php $this->options->themeUrl('/js/auto-render.min.js'); ?>"></script>
+    <?php endif; ?>
+
     <!-- Pjax -->
     <?php if ($this->options->enablepjax == '1'): ?>
         <script defer src="<?php getStaticURL('pjax.min.js'); ?>"></script>
